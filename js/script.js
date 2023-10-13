c5b0acafa0535b3672b24e819582beac61f4eab7
evtisam-dev <evtisamimran@gmail.com>
2023-10-13 19:22:06 +0600

    added information

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const logInButton = document.getElementById("login-button")
-logInButton.addEventListener('click', function(){
+logInButton.addEventListener('click' , function(){
     var logInForm = document.getElementById("login-from");
     logInForm.style.display = 'none';
     var bankingArea = document.getElementById("banking-area");
@@ -17,9 +17,31 @@ depositButton.addEventListener('click', function (){
 
     document.getElementById("deposit-input").value = "";
 })
+// diposit and blance add function 
 function depositAction (id, depositAmount){
     const depositCurrentAmount = document.getElementById(id).innerText;
     const deposiToldAmount  = parseFloat(depositCurrentAmount);
     const totalDepositAmount = depositAmount + deposiToldAmount ;
     document.getElementById(id).innerText = totalDepositAmount;
 }
+// withdraw input
+var withdrawbtn = document.getElementById('withdraw-button');
+withdrawbtn.addEventListener('click', function(){
+    var withdrawInput = document.getElementById('withdraw-input').value;
+    var withdrawInputAmount = parseFloat(withdrawInput);
+// withdraw add
+    var preWithdraw = document.getElementById('pre-withdraw').innerText;
+    var preWithdrawAmount = parseFloat(preWithdraw);
+    var currentWithdrawAmount = withdrawInputAmount + preWithdrawAmount;
+    document.getElementById('pre-withdraw').innerText = currentWithdrawAmount;
+
+    document.getElementById('withdraw-input').value = "";
+
+// remove withdraw amount from total blance
+    var totalBlnc = document.getElementById('total-curnt-blance').innerText;
+    var total = parseFloat(totalBlnc)
+    var mainBlance = total - withdrawInputAmount;
+    document.getElementById('total-curnt-blance').innerText = mainBlance;
+})
+
+
